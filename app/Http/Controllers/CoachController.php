e38ac2cd556ec3789a83d4e6266a40ed3a9f0f70
Hussien El Attar <hussien.elattar1@gmail.com>
2019-03-22 01:23:14 +0200

    Add coach CRUD. Fix db default string length error

--- app/Http/Controllers/CoachController.php
+++ app/Http/Controllers/CoachController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
 use App\Coach;
 use Illuminate\Http\Request;
 
@@ -10,76 +13,99 @@ class CoachController extends Controller
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
+
+        return view('coach.index', compact('coach'));
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function create()
     {
-        //
+        return view('coach.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
     public function store(Request $request)
     {
-        //
+        
+        $requestData = $request->all();
+        
+        Coach::create($requestData);
+
+        return redirect('coach');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Coach  $coach
-     * @return \Illuminate\Http\Response
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
      */
-    public function show(Coach $coach)
+    public function show($id)
     {
-        //
+        $coach = Coach::findOrFail($id);
+
+        return view('coach.show', compact('coach'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Coach  $coach
-     * @return \Illuminate\Http\Response
+     * @param  int  $id
+     *
+     * @return \Illuminate\View\View
      */
-    public function edit(Coach $coach)
+    public function edit($id)
     {
-        //
+        $coach = Coach::findOrFail($id);
+
+        return view('coach.edit', compact('coach'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Coach  $coach
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function update(Request $request, Coach $coach)
+    public function update(Request $request, $id)
     {
-        //
+        
+        $requestData = $request->all();
+        
+        $coach = Coach::findOrFail($id);
+        $coach->update($requestData);
+
+        return redirect('coach');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Coach  $coach
-     * @return \Illuminate\Http\Response
+     * @param  int  $id
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function destroy(Coach $coach)
+    public function destroy($id)
     {
-        //
+        Coach::destroy($id);
+
+        return redirect('coach');
     }
 }

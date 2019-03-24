c440b62f29f3b241a579182368a38e8259b8988a
Ahmed Tarek <a.tarek.elsayed@gmail.com>
2019-03-25 00:11:55 +0200

    Add register function in MembersController

--- app/Http/Controllers/MembersController.php
+++ app/Http/Controllers/MembersController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\MemberRegisterRequest;
+use App\Member;
 
 class MembersController extends Controller
 {
-    //
+    public function register(MemberRegisterRequest $request)
+    {
+        Member::create($request->all());
+        return response()->json([
+            'success' => true,
+            'message' => 'Member created successfully',
+            'data' => $request->all()
+        ], 201);
+    }
 }

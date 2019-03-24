b2841b8dda0a9139ba54afb1d342d6f54a6f8cce
Ahmed Tarek <a.tarek.elsayed@gmail.com>
2019-03-24 23:24:44 +0200

    Add Member model

---
+++ app/Member.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+
+class Member extends Model implements JWTSubject
+{
+    use Notifiable;
+
+    protected $fillable = [
+        'name', 'email', 'password', 'date_of_birth', 'gender'
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+    /**
+     * Get the identifier that will be stored in the subject claim of the JWT.
+     *
+     * @return mixed
+     */
+
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+
+    /**
+     * Return a key value array, containing any custom claims to be added to the JWT.
+     *
+     * @return array
+     */
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+}

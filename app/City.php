39371f6619c11cd497c57fa6a1b4d31dd30bd9a2
Mostafa Mahmoud <mahmodmostafa028@gmail.com>
2019-03-26 21:17:26 +0200

    add city crud

--- app/City.php
+++ app/City.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class City extends Model
 {
-    //
+    protected $fillable = [
+    
+        'name',
+        'country_id',
+    ];
+    public $timestamps = false;
+    public function Country()
+    {
+        // return $this->belongsTo('App\User');
+        return $this->belongsTo(Country::class);
+    }
 }

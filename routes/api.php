d7ce2b2786b1d59a4616c4fd2ea9f128c59a9740
a-tarek <a.tarek.elsayed@gmail.com>
2019-03-27 11:09:55 +0200

    Change url action on update endpoint

--- routes/api.php
+++ routes/api.php
@@ -24,6 +24,7 @@ Route::post('test', 'MembersController@test');
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('member', 'MembersController@member');
+    Route::get('logout', 'MembersController@logout');
     Route::put('update', 'MembersController@update');
     Route::get('sessions', 'MembersController@sessions');
     Route::get('attendance', 'MembersController@attendance');
